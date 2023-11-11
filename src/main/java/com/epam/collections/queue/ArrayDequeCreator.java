518e070743244917475c0f96ab811210fe679cfe
Fanny Dorthey <fannydor@un-k4.kzn.21-school.ru>
2023-11-11 15:41:13 +0300

    hehe boy

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -1,10 +1,30 @@
 package com.epam.collections.queue;
 
 import java.util.ArrayDeque;
+import java.util.Objects;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+
+        ArrayDeque<Integer> arrayDeque = new ArrayDeque<>();
+
+
+        while (!firstQueue.isEmpty() && !secondQueue.isEmpty()) {
+            arrayDeque.add(firstQueue.poll());
+
+
+            arrayDeque.add(firstQueue.poll());
+
+            arrayDeque.add(secondQueue.poll());
+            arrayDeque.add(secondQueue.poll());
+        }
+
+
+
+
+        return arrayDeque;
+
     }
 }
--- src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
+++ src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
@@ -1,9 +1,19 @@
 package com.epam.collections.queue;
 
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class PriorityQueueCreator {
     public PriorityQueue<String> createPriorityQueue(List<String> firstList, List<String> secondList) {
+        PriorityQueue<String> hehe = new PriorityQueue<>(Comparator.reverseOrder());
+
+
+
+        hehe.addAll(firstList);
+        hehe.addAll(secondList);
+
+
+
+
+        return hehe;
     }
 }
